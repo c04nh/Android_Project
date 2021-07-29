57e0f51dc077706f0645b1affd45c3b32b14904e
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-07-29 14:43:24 +0900

    Edit startlayout commit

--- app/src/main/java/kr/hs/emirim/w2038/android_project/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2038/android_project/MainActivity.java
@@ -2,13 +2,27 @@ package kr.hs.emirim.w2038.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnStart, btnLogin, btnCreateAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btn_start);
+        btnLogin = findViewById(R.id.btn_login);
+        btnCreateAccount = findViewById(R.id.btn_create_account);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Layout1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
