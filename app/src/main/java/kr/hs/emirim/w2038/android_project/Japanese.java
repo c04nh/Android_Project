17b72df7cbb595c5734be910aca510c36de230a5
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-08-12 00:45:26 +0900

    activity_japanese.xml(식당 목록 완성) Commit

---
+++ app/src/main/java/kr/hs/emirim/w2038/android_project/Japanese.java
@@ -0,0 +1,27 @@
+package kr.hs.emirim.w2038.android_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Japanese extends AppCompatActivity {
+    Button back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_japanese);
+
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Japanese.this, Layout1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
