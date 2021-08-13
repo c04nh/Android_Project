e344f69e890a9eea452372c59ecb63f133333d9b
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-08-14 02:41:31 +0900

    activity_fusion.xml (식당 목록 완성) Commit

---
+++ app/src/main/java/kr/hs/emirim/w2038/android_project/Fusion.java
@@ -0,0 +1,25 @@
+package kr.hs.emirim.w2038.android_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Fusion extends AppCompatActivity {
+    Button back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fusion);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Fusion.this, Layout1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
