729a5262af6e8103c54d42a96993b7df64fceb49
Song <w2033@e-mirim.hs.kr>
2021-09-14 19:47:43 +0900

    Test

--- app/src/main/java/kr/hs/emirim/w2038/android_project/Korean.java
+++ app/src/main/java/kr/hs/emirim/w2038/android_project/Korean.java
@@ -1,11 +1,14 @@
 package kr.hs.emirim.w2038.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+//import android.widget.Toolbar;
 
 public class Korean extends AppCompatActivity {
     Button back, btn1, btn2;
@@ -20,6 +23,13 @@ public class Korean extends AppCompatActivity {
 
         btn1.setOnClickListener(btnListener);
 
+        //툴바 생성
+        Toolbar toolbar = findViewById(R.id.next_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//뒤로가기
+        getSupportActionBar().setTitle("한식");
+
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,6 +37,15 @@ public class Korean extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
--- app/src/main/java/kr/hs/emirim/w2038/android_project/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2038/android_project/MainActivity.java
@@ -1,12 +1,15 @@
 package kr.hs.emirim.w2038.android_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import androidx.appcompat.widget.Toolbar;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("고시촌에서 뭐 먹지?");
+
         Intent intent = new Intent(this, Splash.class);
         startActivity(intent);
         v_fllipper = findViewById(R.id.image_slide);
@@ -26,5 +35,19 @@ public class MainActivity extends AppCompatActivity {
         v_fllipper.setInAnimation(this,android.R.anim.slide_in_left);
         v_fllipper.setOutAnimation(this,android.R.anim.slide_out_right);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+
+            Intent intent = new Intent(getApplicationContext(),Korean.class);
+            startActivity(intent);
+
+            return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
