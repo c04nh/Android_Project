f17958a6fc7bf293d0d6974ed838e9bf22c7ea0e
Song <w2033@e-mirim.hs.kr>
2021-11-04 20:00:27 +0900

    Edit Korean22.java And Korean23.java Commit

--- app/src/main/java/kr/hs/emirim/w2038/android_project/Korean23.java
+++ app/src/main/java/kr/hs/emirim/w2038/android_project/Korean23.java
@@ -24,7 +24,7 @@ public class Korean23 extends AppCompatActivity implements OnMapReadyCallback{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_korean09);
+        setContentView(R.layout.activity_korean23);
 
         TextView call = findViewById(R.id.call);
 
