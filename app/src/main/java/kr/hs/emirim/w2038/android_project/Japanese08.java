3a99c62cd52bffc66924a0c66a52463ed633bc42
c04nh <w2038@e-mirim.hs.kr>
2021-11-17 14:08:01 +0900

    Edit japanese08, fastfood, fastfood04 Commit

--- app/src/main/java/kr/hs/emirim/w2038/android_project/Japanese08.java
+++ app/src/main/java/kr/hs/emirim/w2038/android_project/Japanese08.java
@@ -56,7 +56,7 @@ public class Japanese08 extends AppCompatActivity implements OnMapReadyCallback{
     public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng location = new LatLng(37.4677324, 126.9207904);
+        LatLng location = new LatLng(37.4687816,126.9349949);
 
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(location);
