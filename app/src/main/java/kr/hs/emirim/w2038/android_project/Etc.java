f124a02db2cf3480a93a3e2754ed9ced268410fc
Song <w2033@e-mirim.hs.kr>
2021-11-18 02:31:42 +0900

    Add Etc06 ~ 07 And Edit Etc.java Commit

--- app/src/main/java/kr/hs/emirim/w2038/android_project/Etc.java
+++ app/src/main/java/kr/hs/emirim/w2038/android_project/Etc.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class Etc extends AppCompatActivity {
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +23,22 @@ public class Etc extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         toolbar.setBackgroundColor(Color.rgb(231, 255, 113));
         toolbar.setTitleTextColor(Color.rgb(100, 98, 98));
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+
+        btn1.setOnClickListener(btnListener);
+        btn2.setOnClickListener(btnListener);
+        btn3.setOnClickListener(btnListener);
+        btn4.setOnClickListener(btnListener);
+        btn5.setOnClickListener(btnListener);
+        btn6.setOnClickListener(btnListener);
+        btn7.setOnClickListener(btnListener);
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
@@ -33,4 +50,40 @@ public class Etc extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            switch (v.getId()){
+                case R.id.btn1:
+                    intent = new Intent(Etc.this, Etc01.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn2:
+                    intent = new Intent(Etc.this, Etc02.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn3:
+                    intent = new Intent(Etc.this, Etc03.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn4:
+                    intent = new Intent(Etc.this, Etc04.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn5:
+                    intent = new Intent(Etc.this, Etc05.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn6:
+                    intent = new Intent(Etc.this, Etc06.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn7:
+                    intent = new Intent(Etc.this, Etc07.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
 }
